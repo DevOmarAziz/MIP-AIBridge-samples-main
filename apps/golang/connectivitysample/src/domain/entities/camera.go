6f562c152bf8a31cbeb47003871185ffa8229f4c
omar.aziz <omar.aziz@iono-tech.com>
2026-05-18 12:10:03 +0300

    refactor: Update Camera struct to include VideoStreams field

--- apps/golang/connectivitysample/src/domain/entities/camera.go
+++ apps/golang/connectivitysample/src/domain/entities/camera.go
@@ -1,6 +1,11 @@
 package entities
 
 type Camera struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID           string        `json:"id"`
+	Name         string        `json:"name"`
+	VideoStreams []VideoStream `json:"videoStreams"`
+}
+
+type VideoStream struct {
+	ID string `json:"id"`
 }
